d224bcb95b7dc67d64408157f9893ee1f46a4684
Grace Looney <gralloon@ut.utm.edu>
2020-02-11 12:22:22 -0600

    Add project files.

---
+++ DatabaseAssignment/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DatabaseAssignment
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection cn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cn = new OleDbConnection("Database21ConnectionString");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = “select* from Assets”;
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = “”;
+            while (read.Read())
+            {
+                data += read[0].ToString() + “\n”;
+            }
+
+        }
+    }
+}
